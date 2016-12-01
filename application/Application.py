74966738ed7506c5717b49896c4adae73ebad8d5
ht5-yaziya <yasin_programmer@hotmail.com>
2016-12-01 09:02:50 +0100

    If needed, now downgrades the graphics open gl to 1.1 (for devices like in our school)

--- application/Application.py
+++ application/Application.py
@@ -2,6 +2,10 @@ from kivy.app import App
 
 from application.screen.ScreenManagement import ScreenManagement
 
+from kivy import Config
+
+Config.set('graphics', 'multisamples', '0')
+
 
 class Application(App):
     def build(self):
