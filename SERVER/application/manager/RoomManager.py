4d3c1fe37073271b70cc210dd6917b6901bc0fa4
Bennet Huch <bennet.huch@tudock.de>
2016-12-02 14:31:58 +0100

    DB and RoomMgr

--- SERVER/application/manager/RoomManager.py
+++ SERVER/application/manager/RoomManager.py
@@ -1,6 +1,10 @@
 from application.lang.Singleton import Singleton
+from application.database.DatabaseConnector import DatabaseConnector
 
 @Singleton
 class RoomManager:
     def __init__(self):
+        return
+
+    def loadRoomsFromDB(self):
         return
