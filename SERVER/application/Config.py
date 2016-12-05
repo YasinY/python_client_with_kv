4454649d48b6df8289c21b53b311d3edebf8ef80
Bennet Huch <bennet@hausbruch.com>
2016-12-05 15:13:58 +0100

    Updated Server

--- SERVER/application/Config.py
+++ SERVER/application/Config.py
@@ -2,3 +2,4 @@ SERVER_IP = '0.0.0.0'
 PORT = 1337
 ENC_BLOCK_SIZE = 16
 DEBUGLEVEL = 0  # 0 = None, 1 = Info, 2 = Error
+BAD_WORDS = ["arsch", "cunt", "huren", "bastard", "hitler", "heil", "reich", "ficken", "adolf", "addi"]
